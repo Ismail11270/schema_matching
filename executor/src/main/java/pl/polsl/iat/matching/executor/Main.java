8333b78ef191d240268f69606c90aaad862d1bfe
Ismail <ismail11270@gmail.com>
2021-08-15 13:15:45 +0400

    Executor setup

---
+++ executor/src/main/java/pl/polsl/iat/matching/executor/Main.java
@@ -0,0 +1,11 @@
+package pl.polsl.iat.matching.executor;
+
+import pl.polsl.iat.matching.schema.model.Schema;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //run executor
+        
+    }
+}
