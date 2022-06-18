68caa7891d9b5c9043826b1fe18f9cc9beefa384
Ismail Atajanov <ismail11270@gmail.com>
2022-06-18 19:56:38 +0200

    Synchronization of POS tagger to fix #3 However this should be fixed in #2 since it's only used by the lemmatizer.

--- app/src/main/java/pl/polsl/iat/matching/processing/PartOfSpeechTagger.java
+++ app/src/main/java/pl/polsl/iat/matching/processing/PartOfSpeechTagger.java
@@ -4,12 +4,14 @@ import pl.polsl.iat.matching.dictionary.nlp.NLPTools;
 import pl.polsl.iat.matching.dictionary.nlp.POSTagger;
 import pl.polsl.iat.matching.dictionary.nlp.POSTag;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class PartOfSpeechTagger implements TextProcessor<Words> {
 
     private POSTagger tagger;
+    private final Object lock = new Object();
 
     public PartOfSpeechTagger() {
         tagger = NLPTools.getPOSTagger();
@@ -18,7 +20,10 @@ public class PartOfSpeechTagger implements TextProcessor<Words> {
     @Override
     public Words process(Words words) {
         if (tagger == null) return words;
-        List<POSTag> tags = tagger.tag(words.wordsAsStrings());
+        List<POSTag> tags = new ArrayList<>();
+        synchronized (lock) {
+             tags.addAll(tagger.tag(words.wordsAsStrings()));
+        }
         IntStream.range(0, tags.size())
                 .forEach(i -> words.get(i).setPos(tags.get(i)));
         return words;
