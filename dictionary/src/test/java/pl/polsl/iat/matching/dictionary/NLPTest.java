7951f400ad0b2a5c3640bc4b564cc5c875aec61e
Ismail Atajanov <ismail11270@gmail.com>
2022-10-05 22:44:59 +0200

    NLP test added, basic test to compare lemmatizer and stemmer

---
+++ dictionary/src/test/java/pl/polsl/iat/matching/dictionary/NLPTest.java
@@ -0,0 +1,29 @@
+package pl.polsl.iat.matching.dictionary;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pl.polsl.iat.matching.dictionary.exception.NlpMildException;
+import pl.polsl.iat.matching.dictionary.nlp.NLPLemmatizer;
+import pl.polsl.iat.matching.dictionary.nlp.NLPStemmer;
+import pl.polsl.iat.matching.dictionary.nlp.NLPTools;
+import pl.polsl.iat.matching.dictionary.nlp.POSTag;
+
+public class NLPTest {
+
+
+    @BeforeAll
+    public static void before() throws NlpMildException {
+        NLPTools.init(false);
+    }
+
+    @Test
+    public void testStemmer() {
+        NLPStemmer stemmer = NLPTools.getStemmer();
+        var a = stemmer.doTheThing("tired");
+        System.out.println(a);
+
+        NLPLemmatizer lemmatizer = NLPTools.getLemmatizer();
+        var b = lemmatizer.doTheThing(new String[]{"tired"}, new POSTag[] {POSTag.JJ});
+        System.out.println(b[0]);
+    }
+}
